35aa8d65dd48bf1bf12a6852ff5b83546a52a3aa
stiven9025 <edistisuarez@hotmail.com>
2016-09-08 16:28:42 -0500

    hola

---
+++ Canon/Models/Facebook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Canon.Models
+{
+    public class Facebook
+    {
+        public int id { get; set; }
+        
+
+
+    }
+}
