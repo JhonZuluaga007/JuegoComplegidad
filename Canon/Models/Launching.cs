dd26b8d65b6b314a027363a5843768b91bc6c611
Zuluaga <Usuario@ZULUAGA>
2016-09-09 14:01:50 -0500

    fixing

---
+++ Canon/Models/Launching.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Canon.Models
+{
+    public class Launching
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdGamer { get; set; }
+        public float Velocidad { get; set; }
+        public float Angle { get; set; }
+    }
+}
